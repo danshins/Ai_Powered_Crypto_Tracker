74a7c1c45dfd3ab6882bc156acf387222c3f2162
Danny <danshinski@icloud.com>
2024-08-19 14:09:31 +0300

    LTSM is bugged, Working on a fix

--- crypto_dashboard/app.py
+++ crypto_dashboard/app.py
@@ -14,7 +14,7 @@ def fetch_crypto_data():
     params = {
         "vs_currency": "usd",
         "order": "market_cap_desc",
-        "per_page": 10,
+        "per_page": 20,
         "page": 1,
         "sparkline": False
     }
